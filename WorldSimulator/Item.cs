ba01bb20778e6a806f617a7eec913f5782760a10
Petr Kotáb <petrkotab99@gmail.com>
2023-08-10 21:13:00 +0200

    Resource items.

---
+++ WorldSimulator/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WorldSimulator;
+internal class ItemType
+{
+}
+
+internal static class ItemTypes
+{
+    public readonly static ItemType Wood = new();
+    public readonly static ItemType Stone = new();
+    public readonly static ItemType Iron = new();
+    public readonly static ItemType Meat = new();
+
+    public static IEnumerable<ResourceType> GetAllTypes()
+        => typeof(ItemTypes).GetFields().Select(p => (ResourceType)p.GetValue(null));
+}
--- WorldSimulator/ResourceType.cs
+++ WorldSimulator/ResourceType.cs
@@ -4,14 +4,36 @@ using System.Linq;
 namespace WorldSimulator;
 internal class ResourceType
 {
+    /// <summary>
+    /// Item obtained by harvesting resource.
+    /// </summary>
+    public ItemType HarvestItem { get; init; }
+
+    public int HarvestQuantity { get; init; }
 }
 
 internal static class ResourceTypes
 {
-    public readonly static ResourceType Tree = new();
-    public readonly static ResourceType Rock = new();
-    public readonly static ResourceType Deposite = new();
-    public readonly static ResourceType Deer = new();
+    public readonly static ResourceType Tree = new()
+    {
+        HarvestItem = ItemTypes.Wood,
+        HarvestQuantity = 1,
+    };
+    public readonly static ResourceType Rock = new()
+    {
+        HarvestItem = ItemTypes.Stone,
+        HarvestQuantity = 1,
+    };
+    public readonly static ResourceType Deposite = new()
+    {
+        HarvestItem = ItemTypes.Iron,
+        HarvestQuantity = 1,
+    };
+    public readonly static ResourceType Deer = new()
+    {
+        HarvestItem = ItemTypes.Meat,
+        HarvestQuantity = 1,
+    };
 
     public static IEnumerable<ResourceType> GetAllTypes()
         => typeof(ResourceTypes).GetFields().Select(p => (ResourceType)p.GetValue(null));
