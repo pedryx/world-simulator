f4a0489c60d8b299eec67b15801119f4174df53b
Petr Kotáb <petrkotab99@gmail.com>
2023-11-01 18:55:56 +0100

    Added ECS benchmarks.

---
+++ WorldSimulator.BenchMarks/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+using WorldSimulator.Benchmarks;
+
+BenchmarkRunner.Run<ECSBenchmarks>(ManualConfig
+    .Create(DefaultConfig.Instance)
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator));
